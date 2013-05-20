6125c499753e1089d19e505938797266cff2efda
tenix <tenix@foxmail.com>
2013-05-20 13:30:36 +0800

    [linux_driver]ex01 Owner:Tenix XU Description:git add Build number: Issue: Solution: UTS No.: The root cause of SVN commit :Function implementation

---
+++ linux_android_basic_training/linux_drivers_training/ex_01/helloworld.c
@@ -0,0 +1,24 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+static int helloworld(void)
+{
+  printk(KERN_ALERT "hello world\n");
+  return 0;
+}
+
+static void exit_hello(void)
+{
+  printk(KERN_ALERT "goodbye kernel");
+}
+
+
+
+module_init(helloworld);
+module_exit(exit_hello);
+
+
+MODULE_AUTHOR("Tenix xu");
+MODULE_LICENSE("Dual BSD/GPL");
+MODULE_DESCRIPTION("exercise 01");
+MODULE_ALIAS("an example");
