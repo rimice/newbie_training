0bd23538bed5b2d18adf85391f7057fabb239f95
Tenix_Xu <Tenix_Xu@wistron.com>
2013-05-28 17:03:10 +0800

    [drivers_training] ex01_update
    owner: Tenix Xu
    Issue: Coding Style
    Description: ex01_update
    Root Cause: Coding Style

--- linux_android_basic_training/linux_drivers_training/ex_01/helloworld.c
+++ linux_android_basic_training/linux_drivers_training/ex_01/helloworld.c
@@ -1,12 +1,40 @@
+/*****************************************************
+** Filename: helloworld.c
+** Copyright (c) 2013 Wistron
+** All rights reserved.
+**
+** Description:
+** This is a simple kernel module.
+** Author/Created :
+** Tenix Xu, May 24'13
+** Modification History:
+** Tenix Xu, May 28'13
+**
+****************************************************/
+
+/**********************************************
+*** Include Files
+**********************************************/
 #include <linux/init.h>
 #include <linux/module.h>
 
+
+/**********************************************
+***Description:
+*** helloword function
+**********************************************/
 static int helloworld(void)
 {
   printk(KERN_ALERT "hello world\n");
   return 0;
 }
 
+
+
+/*******************************************
+***Description:
+*** exit function
+*********************************************/
 static void exit_hello(void)
 {
   printk(KERN_ALERT "goodbye kernel");
@@ -15,7 +43,9 @@ static void exit_hello(void)
 module_init(helloworld);
 module_exit(exit_hello);
 
-
+/*
+*Statement and description of the module
+*/
 MODULE_AUTHOR("Tenix xu");
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_DESCRIPTION("exercise 01");
