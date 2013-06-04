4c44ae44f3addccb5e5714c8d9205f70b753e4ba
Tenix_Xu <Tenix_Xu@wistron.com>
2013-06-04 18:28:52 +0800

    [drivers_training] ex02_update
    owner: Tenix Xu
    Issue: code style
    Description: ex02_update
    Root Cause: code style

--- linux_android_basic_training/linux_drivers_training/ex_02/app.c
+++ linux_android_basic_training/linux_drivers_training/ex_02/app.c
@@ -25,41 +25,40 @@ int main(void)
 {
 	char buf[20];
 	int fd;
-        int ret;
+	int ret;
 	fd = open("/dev/ex02", O_RDWR);//打开       
 	if(fd < 0)
 	{
 		printf("open is error\n");
 		return -1;
 	}
-        printf("open is ok\n");
+	printf("open is ok\n");
 
 	ret=write(fd, "Tenix Xu", 10);//写入
-        if(ret!= 10)
-           {
-             printf(" writing may error\n");
-             exit(1);
-            }   
-   
+	if(ret!= 10)
+	{
+		printf(" writing may error\n");
+		exit(1);
+	}   
+
 	ret=read(fd, buf, 10);//读出
 	if (ret <0 )
-	   {
-	     printf("Read is error\n");
-	     exit(1);
-	   }
-        printf("buf is [%s]\n", buf);
+	{
+		printf("Read is error\n");
+		exit(1);
+	}
+	printf("buf is [%s]\n", buf);
 
-        ioctl(fd,ex02_CLEAR);  //再清空 
-        ret=read(fd, buf, 10);
+	ioctl(fd,ex02_CLEAR);  //再清空 
+	ret=read(fd, buf, 10);
 	if (ret <0 )
-	   {
-	     printf("Read is error\n");
-	     exit(1);
-	   } 
-        printf("buf is [%s]\n", buf);//读出清空后的buf
-     
+	{
+		printf("Read is error\n");
+		exit(1);
+	} 
+	printf("buf is [%s]\n", buf);//读出清空后的buf     
 	close(fd); 
-        printf("close is ok\n");
+	printf("close is ok\n");
 	return 0;
 }
 
